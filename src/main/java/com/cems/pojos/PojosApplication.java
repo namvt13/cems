8f668fc6cc47dc5264e6db2e7a597a58aa4aadc5
Tan Phung <tan.phung@vexere.com>
2020-11-21 10:52:32 +0700

    First commit

---
+++ src/main/java/com/cems/pojos/PojosApplication.java
@@ -0,0 +1,13 @@
+package com.cems.pojos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PojosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PojosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cems/pojos/PojosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cems.pojos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PojosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
